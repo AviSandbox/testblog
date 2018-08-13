9f023a6cc318bb3a04b667a141008a25c4c4d857
avisandbox <yiyuan.new@gmail.com>
2018-08-13 17:04:30 +0300

    no message

--- my_blog/my_blog/settings.py
+++ my_blog/my_blog/settings.py
@@ -31,8 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    #'bootstrap_admin',
-    'bootstrap4',
+   
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
